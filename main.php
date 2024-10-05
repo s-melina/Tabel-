e50ee2a5a040f80101cad974e8d0734234c4a93d
s-melina <161036882+s-melina@users.noreply.github.com>
2024-10-05 07:57:04 +0330

    Initial commit

---
+++ main.php
@@ -0,0 +1,137 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<nav class="navbar bg-body-tertiary mb-4">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="web.a.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+            table
+        </a>
+    </div>
+</nav>
+
+<div class="container-md">
+    <table class="table table-hover">
+        <thead>
+        <tr class="table-warning">
+            <th scope="col">No.</th>
+            <th scope="col">Category</th>
+            <th scope="col">Time Taken</th>
+            <th scope="col">Score</th>
+            <th scope="col">Status</th>
+            <th scope="col">Open</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>Bootstrap Container</td>
+            <td>0 s</td>
+            <td>0/6</td>
+            <td>
+                <img src="tick.png" alt="Bootstrap" width="25" height="25">
+            </td>
+            <td>
+                <img src="opendoor.png" alt="Bootstrap" width="25" height="25">
+            </td>
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td>Bootstrap Colors and Images</td>
+            <td>0 s</td>
+            <td>0/6</td>
+            <td>
+                <img src="tick.png" alt="Bootstrap" width="25" height="25">
+            </td>
+            <td>
+                <img src="opendoor.png" alt="Bootstrap" width="25" height="25">
+            </td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td>Bootstrap Tables and Alerts</td>
+            <td>0 s</td>
+            <td>0/6</td>
+            <td>
+                <img src="tick.png" alt="Bootstrap" width="25" height="25">
+            </td>
+            <td>
+                <img src="opendoor.png" alt="Bootstrap" width="25" height="25">
+            </td>
+        </tr>
+        <tr>
+            <th scope="row">4</th>
+            <td>Bootstrap Button, Badges and ProgressBar</td>
+            <td>0 s</td>
+            <td>0/8</td>
+            <td>
+                <img src="tick.png" alt="Bootstrap" width="25" height="25">
+            </td>
+            <td>
+                <img src="opendoor.png" alt="Bootstrap" width="25" height="25">
+            </td>
+        </tr>
+        <tr>
+            <th scope="row">5</th>
+            <td>Bootstrap Spinner and Alerts </td>
+            <td>0 s</td>
+            <td>0/5</td>
+            <td>
+                <img src="tick.png" alt="Bootstrap" width="25" height="25">
+            </td>
+            <td>
+                <img src="opendoor.png" alt="Bootstrap" width="25" height="25">
+            </td>
+        </tr>
+        <tr>
+            <th scope="row">6</th>
+            <td>Bootstrap List Groups and Cards</td>
+            <td>0 s</td>
+            <td>0/4</td>
+            <td>
+                <img src="tick.png" alt="Bootstrap" width="25" height="25">
+            </td>
+            <td>
+                <img src="opendoor.png" alt="Bootstrap" width="25" height="25">
+            </td>
+        </tr>
+        <tr>
+            <th scope="row">7</th>
+            <td>Bootstrap Dropdowns and Collapse</td>
+            <td>0 s</td>
+            <td>0/2</td>
+            <td>
+                <img src="tick.png" alt="Bootstrap" width="25" height="25">
+            </td>
+            <td>
+                <img src="opendoor.png" alt="Bootstrap" width="25" height="25">
+            </td>
+        </tr>
+        <tr>
+            <th scope="row">8</th>
+            <td>Bootstrap Navs and navbar</td>
+            <td>0 s</td>
+            <td>0/5</td>
+            <td>
+                <img src="tick.png" alt="Bootstrap" width="25" height="25">
+            </td>
+            <td>
+                <img src="opendoor.png" alt="Bootstrap" width="25" height="25">
+            </td>
+        </tr>
+
+        </tbody>
+    </table>
+</div>
+
+
+<script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
